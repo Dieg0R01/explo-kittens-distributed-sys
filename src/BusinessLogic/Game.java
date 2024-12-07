253e820ed215cd951a496ff7e99957218408049a
diazpabloo <pacerim@unirioja.es>
2024-12-07 12:06:39 +0100

    7/12 - Game start with parameter room

--- src/BusinessLogic/Game.java
+++ src/BusinessLogic/Game.java
@@ -8,6 +8,7 @@ import DomainModel.CardStack;
 import DomainModel.DiscardDeck;
 import DomainModel.MainDeck;
 import DomainModel.Player;
+import Server.GameLogic.Room;
 
 public class Game{
 
@@ -24,9 +25,6 @@ public class Game{
 		priorityManager = PriorityManager.getInstance();
 		// TurnManager.InstantiateLogger(); !OJO¡ QUE HACEMOS CON LOS LOGGERS
 		turnManager = TurnManager.getInstance();
-    }
-    protected Game(int n){
-        
     }
     /**
      * @return MainDeck return the mainDeck
@@ -104,6 +102,9 @@ public class Game{
         this.stack = stack;
     }
 
+    /* 
+    NOT USED
+    
     public void start(int numPlayers) {
 		if (numPlayers > 2 || numPlayers < 5) {
 
@@ -117,8 +118,24 @@ public class Game{
 		}
 		
 	}
+    */
 
-    public void start(List<Players> players){
+    public void start(Room room) {
+        int numPlayers = room.getPlayersList().size();
+		if (numPlayers > 2 || numPlayers < 5) {
+
+			mainDeck.initStartingDeck();
+            playerManager = new PlayerManager();
+            playerManager.setPlayers(room.getPlayersList());
+            priorityManager.addPlayers(playerManager.getPlayers());
+            playerManager.distributePlayersInitialHand();
+            mainDeck.populateDeck(numPlayers);
+            turnManager.setPlayerManager(playerManager);
+		}
+		
+	}
+
+    public void start(List<Player> players){
         
     }
 
